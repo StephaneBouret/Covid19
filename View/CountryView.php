0ff809f0f506625fc42e53f29ee22184a329350d
StephaneBouret <sbouret@orange.fr>
2020-03-27 13:57:02 +0100

    deuxieme commit

--- View/CountryView.php
+++ View/CountryView.php
@@ -107,7 +107,7 @@ class CountryView extends View
             }
             $tauxInfection = round((($countries['Infections'] / $countries['population'])*100), 2);
             $this->page .= "<tr><th scope='row' class='align-middle'>".date("d-m-Y", $countries['Date'])."</th>"
-                        ."<td class='align-middle'><a href='index.php?controller=country&action=getCountry&pays=".$countries['name']."'>".$countries['name']." <i class='fas fa-sign-in-alt'></i></a></td>"
+                        ."<td class='align-middle'><a class='d-block' href='index.php?controller=country&action=getCountry&pays=".$countries['name']."'><img src='./png100px/".$countries['code'].".png'>"." ".$countries['name']." <i class='fas fa-sign-in-alt'></i></a></td>"
                         ."<td class='align-middle'>".$countries['Infections']."</td>"
                         ."<td class='align-middle'>".$countries['Deces']."</td>"
                         ."<td class='align-middle'>".$countries['Guerisons']."</td>"
@@ -128,9 +128,15 @@ class CountryView extends View
      * @return void
      */
     public function modal($country,$pays){
-        // var_dump($country);
         $this->page .= "<div class='container'>";
-        $this->page .= "<h2 class='mt-4 mb-4'>Derniers chiffre du Coronavirus (Covid19) - Pays : ".$pays."</h2>";
+        foreach ($country as $key => $value) {
+            $nomPays = $value['name'];
+            $codePays = $value['code'];
+            $population = $value['population'];
+        }
+        $this->page .= "<div class='text-center'><img class='rounded mx-auto d-block' src='./png100px/".$codePays.".png'>
+                    <p class='mt-4'><i class='fa fa-users mr-1'></i>".$population." habitants</p></div>";
+        $this->page .= "<h2 class='mt-4 mb-4'>Derniers chiffres du Coronavirus (Covid19) - Pays : ".$nomPays." ( ".$codePays." )</h2>";
         $this->page .= "<table class='table table-sm'>";
         $this->page .= "<thead class='thead-dark'><tr>";
         $this->page .= "<th scope='col' class='align-middle'>Date</th>";
